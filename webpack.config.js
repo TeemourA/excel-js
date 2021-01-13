670ab6c9dcd3f97ce70fd0fdc158e0088165269d
TeemourA <taxprogramming@gmail.com>
2021-01-13 21:53:50 +0300

    finish project config

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,31 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const isProd = process.env.NODE_ENV === 'production';
+const isDev = !isProd;
+
+const getFilename = ext =>
+  isDev ? `bundle.${ext}` : `bundle.[contenthash].${ext}`;
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  ];
+
+  return loaders;
+};
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: './index.js',
+  entry: ['@babel/polyfill', './index.js'],
   output: {
-    filename: 'bundle.[contenthash].js',
+    filename: getFilename('js'),
     path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
@@ -19,10 +38,19 @@ module.exports = {
       '@core': path.resolve(__dirname, 'src/core'),
     },
   },
+  devtool: isDev ? 'source-map' : false,
+  devServer: {
+    port: 3000,
+    hot: isDev,
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
       template: 'index.html',
+      // minify: {
+      //   removeComments: isProd,
+      //   collapseWhitespace: isProd,
+      // },
     }),
     new CopyPlugin({
       patterns: [
@@ -33,7 +61,27 @@ module.exports = {
       ],
     }),
     new MiniCssExtractPlugin({
-      filename: 'bundle.[contenthash].css',
+      filename: getFilename('css'),
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+      },
+    ],
+  },
 };
